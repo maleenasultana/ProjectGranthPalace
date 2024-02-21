10b116df69af8a1bb82680cf02890412bf56bf0f
Maleena <maleena.sohail@gmail.com>
2024-02-21 20:00:23 +0530

    ch

--- src/Components/Tabs.js
+++ src/Components/Tabs.js
@@ -8,14 +8,15 @@ const Tabs = () => {
   return (
     <div className='bg'>
         
-        <Button colorScheme='purple' size="md" >New Arrivals</Button>
-        <Button colorScheme='blue' size="md" >MBBS</Button>
-        <Button colorScheme='purple' size="md" >Post Graduation</Button>
-        <Button colorScheme='blue' size="md" >Super Speciality</Button>
-        <Button colorScheme='purple' size="md" >Medical College</Button>
-        <Button colorScheme='blue' size="md" >Courses</Button>
-        <Button colorScheme='purple' size="md" >Know Your College</Button>
-       
+        <Button mx={2} colorScheme='purple' size="md" >New Arrivals</Button> 
+        <Button mx={1} px={6} colorScheme='blue' size="md" >MBBS</Button>
+        <Button mx={1}colorScheme='purple' size="md" >Post Graduation</Button>
+        <Button mx={1} colorScheme='blue' size="md" >Super Speciality</Button>
+        <Button mx={1} colorScheme='purple' size="md" >Medical College</Button>
+        <Button mx={1} px={6} colorScheme='blue' size="md" >Courses</Button>
+        <Button mx={1} colorScheme='purple' size="md" >Know Your College</Button>
+        <Button mx={1} colorScheme='purple' size="md" >News and Updates</Button>
+        <Button mx={1} px={6} colorScheme='purple' size="md" >contact</Button>
         {/* <SearchIcon color="white" spacing={7}/> */}
       
     </div>
